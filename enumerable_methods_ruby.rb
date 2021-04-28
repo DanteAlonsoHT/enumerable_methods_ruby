b33c0ada8e1c614d2bbc23c8784fe2c71481f713
DanteAlonsoHT <dante1818@hotmail.com>
2021-04-28 09:32:02 -0500

    Add my_none function

--- enumerable_methods_ruby.rb
+++ enumerable_methods_ruby.rb
@@ -21,9 +21,30 @@ module Enumerable
     return to_enum(:my_select) unless block_given?
 
     new_array = []
-    to_a.my_each do |x|
-      new_array.push(x) if yield x
+    to_a.my_each do |i|
+      new_array.push(i) if yield i
     end
     new_array
+  end
+
+  def my_none?(param = nil)
+    if !block_given? && param.nil?
+      to_a.my_each { |x| return true if i == false || x.nil? }
+      return false
+    elsif !block_given? && param.is_a?(Class)
+      to_a.my_each { |i| return false if i.instance_of?(param) }
+      return true
+    elsif !block_given? && param.instance_of?(Regexp)
+      to_a.my_each { |i| return false if i.match(param) }
+      return true
+    elsif !block_given? && !param.nil?
+      to_a.my_each { |i| return false if i == param }
+    elsif block_given? && param.instance_of?(Range)
+      to_a.my_each { |i| return false if yield i }
+      return true
+    else
+      to_a.my_each { |i| return false if yield i }
+    end
+    true
   end
 end
