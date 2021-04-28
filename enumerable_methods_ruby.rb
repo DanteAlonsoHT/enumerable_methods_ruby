dd8c9e7c795e917a9d8f56eec56638e00eccdd84
DanteAlonsoHT <dante1818@hotmail.com>
2021-04-28 09:43:09 -0500

    Improve my_inject function

--- enumerable_methods_ruby.rb
+++ enumerable_methods_ruby.rb
@@ -77,5 +77,10 @@ module Enumerable
 
     reduce = param[0] if param[0].is_a?(Integer)
     operator = param[0].is_a?(Symbol) ? param[0] : param[1]
+    
+    if operator
+      collection.my_each { |i| reduce = reduce ? reduce.send(operator, i) : i }
+      return reduce
+    end
   end
 end
