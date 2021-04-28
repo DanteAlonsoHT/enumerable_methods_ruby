3441369955dd37d60208a4a8094830e280e747a8
DanteAlonsoHT <dante1818@hotmail.com>
2021-04-28 09:28:02 -0500

    Add my_each_with_index function

--- enumerable_methods_ruby.rb
+++ enumerable_methods_ruby.rb
@@ -7,4 +7,13 @@ module Enumerable
     end
     self
   end
+  
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    to_a.length.times do |i|
+      yield to_a[i], i
+    end
+    self
+  end
 end
